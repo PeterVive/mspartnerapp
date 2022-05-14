200fe0d8e7bfa29c1660a6aebef99d26547c1523
Peter Erritsø Vive <petervive@outlook.com>
2022-05-14 15:31:38 +0200

    Remove cors

--- utils/cors.js
+++
@@ -1,6 +0,0 @@
-export const corsOptions = {
-  // Options
-  methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-  origin: '*',
-  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-};
