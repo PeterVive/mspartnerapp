4d0e7987ea72cf441a7f9e7c6ae91fa706e54965
Peter Erritsø Vive <petervive@outlook.com>
2022-05-14 11:45:29 +0200

    fix

--- components/CustomToolbar.js
+++ components/CustomToolbar.js
@@ -3,7 +3,10 @@ import { GridToolbarContainer, GridToolbarExport } from "@mui/x-data-grid";
 export function CustomToolbar() {
   return (
     <GridToolbarContainer>
-      <GridToolbarExport printOptions={{ disableToolbarButton: true }} />
+      <GridToolbarExport
+        printOptions={{ disableToolbarButton: true }}
+        csvOptions={{ utf8WithBom: true, fileName: "MSPartnerApp_Export" }}
+      />
     </GridToolbarContainer>
   );
 }
--- pages/api/tenants/[tenantid]/groups/[groupid].js
+++ pages/api/tenants/[tenantid]/groups/[groupid].js
@@ -1,24 +1,18 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import { MyAuthenticationProvider } from '../../../../../utils/customAuthProvider';
-import { Client, ClientOptions } from '@microsoft/microsoft-graph-client';
-import { Group } from '@microsoft/microsoft-graph-types-beta';
-import { corsOptions } from '../../../../../utils/cors';
-import NextCors from 'nextjs-cors';
+/* eslint-disable import/no-anonymous-default-export */
+import { MyAuthenticationProvider } from "../../../../../utils/customAuthProvider";
+import { Client } from "@microsoft/microsoft-graph-client";
 
-export default async (_: NextApiRequest, res: NextApiResponse) => {
-  await NextCors(_, res, corsOptions);
-
-  let clientOptions: ClientOptions = {
-    defaultVersion: 'beta',
+export default async (_, res) => {
+  let clientOptions = {
+    defaultVersion: "beta",
     authProvider: new MyAuthenticationProvider(
-      _.query.tenantid as string,
-      ['https://graph.microsoft.com/.default'],
+      _.query.tenantid,
+      ["https://graph.microsoft.com/.default"],
       false
-    )
+    ),
   };
 
   const client = Client.initWithMiddleware(clientOptions);
-  const group = (await client.api(`/groups/${_.query.groupid}`).get())
-    .value as Group;
+  const group = (await client.api(`/groups/${_.query.groupid}`).get()).value;
   res.status(200).json(group);
 };
