6badb09eb5480f4fe6d1ade9d69d7bd080b34b5d
Peter Erritsø Vive <petervive@outlook.com>
2022-05-28 04:03:10 +0200

    fix

--- next.config.js
+++ next.config.js
@@ -1,8 +1,4 @@
-const withBundleAnalyzer = require("@next/bundle-analyzer")({
-  enabled: process.env.ANALYZE === "true",
-});
-
-module.exports = withBundleAnalyzer({
+module.exports = {
   swcMinify: true,
   reactStrictMode: true,
   experimental: {
@@ -30,4 +26,4 @@ module.exports = withBundleAnalyzer({
       },
     ];
   },
-});
+};
